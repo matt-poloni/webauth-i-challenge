ca3aabb39c2b14e9ffbe4aa55058cb812b166965
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-06 16:08:45 -0400

    Uses knex migration to build users table

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
 
 // const registerRt = require('./register');
 // const loginRt = require('./login');
