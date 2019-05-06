8586babfc93f3f87a06c153c96dd8110b9c79ede
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-06 15:49:14 -0400

    Adds server.js & index.js

---
+++ api/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+// const registerRt = require('./register');
+// const loginRt = require('./login');
+// const usersRt = require('./users');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+// server.use('/api/register/', registerRt);
+// server.use('/api/login/', loginRt);
+// server.use('/api/users/', usersRt);
+
+server.get('/', (req, res) => {
+  res.send("Up and running.");
+});
