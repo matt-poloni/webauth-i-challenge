fe6431b3c11e122e99caaf2249af10922bbf3e3d
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-06 16:29:50 -0400

    Completes /register POST

---
+++ api/register/registerModel.js
@@ -0,0 +1,10 @@
+const tbl = 'users';
+const basic = require('../basicModel')(tbl);
+const db = require('../../data/dbConfig');
+
+module.exports = {
+  post: async function(creds) {
+    const [id] = await basic.post(creds);
+    return basic.get(id);
+  },
+}
--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-// const registerRt = require('./register');
+const registerRt = require('./register');
 // const loginRt = require('./login');
 // const usersRt = require('./users');
 
@@ -12,7 +12,7 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-// server.use('/api/register/', registerRt);
+server.use('/api/register/', registerRt);
 // server.use('/api/login/', loginRt);
 // server.use('/api/users/', usersRt);
 
